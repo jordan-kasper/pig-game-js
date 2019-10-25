2e2096817a116140c6d264895e6df829900c148a
Jordan Kasper <jordan.kasper@gmail.com>
2019-10-25 15:13:09 -0700

    adding roll logic for score

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
+dice = Math.floor(Math.random() * 6) + 1;
+
 document.querySelector('#current-' + activePlayer).textContent = dice;
 // document.querySelector('#current-' + activePlayer).innerHTML = '<em>'+dice+'</em>'
 
@@ -32,4 +34,20 @@ document.querySelector('.btn-roll').addEventListener('click',function(){
     diceDom.style.display = 'block';
     diceDom.src = 'images/dice-' + dice +'.png'
 
+    if(dice !== 1) {
+        roundScore += dice;
+        document.querySelector('#current-'+ activePlayer).textContent = roundScore;
+    }
+    else{
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
+        roundScore = 0;
+
+        document.querySelector('#current-0').textContent = 0;
+        document.querySelector('#current-1').textContent = 0;
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        document.querySelector('.dice').style.display = 'none';
+    }
 });
